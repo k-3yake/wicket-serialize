0f4682d387fe609777badffe8848c22c48e3edb5
katsuki miyake <k3yake@gmail.com>
2015-05-28 07:54:37 +0900

    フォーマット修正

--- src/main/java/org/k3yake/HomePage.java
+++ src/main/java/org/k3yake/HomePage.java
@@ -21,47 +21,26 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class HomePage extends WebPage {
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = 1L;
 
-	/** click count for Link. */
 	private int linkClickCount = 0;
 
-	/**
-	 * Construct.
-	 */
-	public HomePage()
-	{
+	public HomePage(){
 		add(new Label("message", new SessionModel()));
-		//add(new BookmarkablePageLink<>("indexLink", Index.class));
 
-		// Action link counts link clicks
-		final Link actionLink = new Link("actionLink")
-		{
+		final Link actionLink = new Link("actionLink"){
 			@Override
-			public void onClick()
-			{
+			public void onClick(){
 				linkClickCount++;
 			}
 		};
 		add(actionLink);
-		actionLink.add(new Label("linkClickCount", new PropertyModel<Integer>(this,
-				"linkClickCount")));
-
+		actionLink.add(new Label("linkClickCount", new PropertyModel<Integer>(this,"linkClickCount")));
 		final TextField<String> field = new TextField<String>("textfield", new Model<String>());
-
-		StatelessForm<?> statelessForm = new StatelessForm("statelessform")
-		{
-			/**
-			 * @see org.apache.wicket.markup.html.form.Form#onSubmit()
-			 */
+		StatelessForm<?> statelessForm = new StatelessForm("statelessform")	{
 			@Override
-			protected void onSubmit()
-			{
-				info("Submitted text: " + field.getDefaultModelObject() + ", link click count: " +
-						linkClickCount);
+			protected void onSubmit(){
+				info("Submitted text: " + field.getDefaultModelObject() + ", link click count: " +	linkClickCount);
 			}
 		};
 		statelessForm.add(field);
@@ -69,24 +48,7 @@ public class HomePage extends WebPage {
 		add(new FeedbackPanel("feedback"));
 	}
 
-	/**
-	 * Gets linkClickCount.
-	 *
-	 * @return linkClickCount
-	 */
-	public int getLinkClickCount()
-	{
-		return linkClickCount;
-	}
+	public int getLinkClickCount(){		return linkClickCount;	}
 
-	/**
-	 * Sets linkClickCount.
-	 *
-	 * @param linkClickCount
-	 *            linkClickCount
-	 */
-	public void setLinkClickCount(int linkClickCount)
-	{
-		this.linkClickCount = linkClickCount;
-	}
+	public void setLinkClickCount(int linkClickCount){		this.linkClickCount = linkClickCount;	}
 }
